9db7d171dc5336f66e46eb6d55ca58e230c841ad
Agustín Bravo Pérez <anbravoperez@gmail.com>
2024-06-12 00:49:49 -0300

    agregado reporte de test coverage con vitest

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ["src/**/*.{test,spec}.{js,ts}"],
 		environment: "jsdom",
-		globals: true
+		globals: true,
+		include: ["src/**/*.{test,spec}.{js,ts}"],
+		coverage: {
+			reporter: ["text", "json", "html"],
+			include: ["**/src/**/*.{svelte,js,ts}"],
+		}
 	}
 });
